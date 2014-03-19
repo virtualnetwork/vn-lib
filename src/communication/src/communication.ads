9ca17f2dea45ea413fad9c754fd4cb425543e13d
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-03-19 14:35:37 +0100

    Updated the communication package.

--- src/communication/src/communication.ads
+++ src/communication/src/communication.ads
@@ -3,22 +3,23 @@ with VN_Message;
 
 package Communication is
 
-   type Type_Buffer is range 1 .. 10;
+   type VN_Message_Buffer is range 1 .. 10;
+
    type Com is abstract tagged private;
 
    procedure Send(This: in out Com;
                   Message: in VN_Message.VN_Message_Basic)
                         is abstract;
 
-   function Receive( This: in out Com;
+   procedure Receive( This: in out Com;
                      Message: out VN_Message.VN_Message_Basic;
                      Is_Received: out Boolean)
                         is abstract;
 private
 
-   type Com is new Ada.Finalization.Controlled with
+   type Com is abstract new Ada.Finalization.Controlled with
       record
-         Buffer: Type_Buffer := 1;
+         Buffer: VN_Message_Buffer := 1;
       end record;
 
 end Communication;
