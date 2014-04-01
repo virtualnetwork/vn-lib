1219fb9732f1fde27bbf1e0f89a47fc68f400333
Nils <nils@ubuntu.(none)>
2014-04-01 08:40:25 +0200

    Added test code to show compilation error

--- src/vn/communication/can/task_test/src/task_test.ads
+++ src/vn/communication/can/task_test/src/task_test.ads
@@ -4,24 +4,32 @@
 -- Date: 2014-XX-XX
 -- Summary:
 
-With VN.Communication.CAN.Can_Task;
-with VN.Communication.CAN.CAN_Interface;
-with Ada.Real_Time;
-with System;
+--  With VN.Communication.CAN.Can_Task;
+--  with VN.Communication.CAN.CAN_Interface;
+--  with Ada.Real_Time;
+--  with System;
 
 package Task_Test is
 
+   type VN_Message_Basic is tagged private;
 
-   thePeriod : aliased Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(100);
+--     thePeriod : aliased Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(100);
+--
+--     U1 : aliased VN.Communication.CAN.UCID := 1;
+--     C1 : aliased VN.VN_CUUID := (1, others => 5);
+--
+--     myInterface : aliased VN.Communication.CAN.CAN_Interface.CAN_Interface_Type
+--       (U1'Unchecked_Access, C1'Unchecked_Access, VN.Communication.CAN.CAN_Interface.SM_CAN);
+--
+--     myTask : aliased VN.Communication.CAN.Can_Task.CAN_Task_Type
+--       (myInterface'Access, System.Priority'Last, thePeriod'Access);
 
-   U1 : aliased VN.Communication.CAN.UCID := 1;
-   C1 : aliased VN.VN_CUUID := (1, others => 5);
+private
+   type VN_Message_Basic is tagged --new Ada.Finalization.Controlled with
+      record
+      --   Header   : VN_Header;
+      --    Checksum : VN_Checksum;
 
-   myInterface : aliased VN.Communication.CAN.CAN_Interface.CAN_Interface_Type
-     (U1'Unchecked_Access, C1'Unchecked_Access, VN.Communication.CAN.CAN_Interface.SM_CAN);
-
-   myTask : aliased VN.Communication.CAN.Can_Task.CAN_Task_Type
-     (myInterface'Access, System.Priority'Last, thePeriod'Access);
-
-
+         temp : Integer;
+      end record;
 end Task_Test;
--- src/vn/communication/can/task_test/src/task_test_main.adb
+++ src/vn/communication/can/task_test/src/task_test_main.adb
@@ -5,6 +5,8 @@ With Task_Test;
 with VN;
 
 procedure Task_Test_Main is
+
+
 begin
 
    VN.Text_IO.Put_Line("Hello world");
