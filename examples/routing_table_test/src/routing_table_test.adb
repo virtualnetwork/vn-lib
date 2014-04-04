cf2de562c2076c881e0bd3fbf5cb28f4351684e2
Nils <nils@ubuntu.(none)>
2014-04-04 14:25:53 +0200

    Made the tests more extensive

--- examples/routing_table_test/src/routing_table_test.adb
+++ examples/routing_table_test/src/routing_table_test.adb
@@ -7,44 +7,61 @@ with VN.Communication.CAN;
 use VN.Communication.CAN;
 with VN.Communication.Routing_Table;
 
+with Interfaces;
+use Interfaces;
+
 procedure Routing_Table_Test is
 
    package pack is new VN.Communication.Routing_Table(VN.Communication.CAN.CAN_Address_Sender);
    table : pack.Table_Type(VN.VN_Logical_Address(1000));
 
-   found : boolean;
 
-   canAddressIn, canAddressOut : VN.Communication.CAN.CAN_Address_Sender;
-   logAddress : VN.VN_Logical_Address;
+   passed : Boolean := true;
+ --  canAddressIn, canAddressOut : VN.Communication.CAN.CAN_Address_Sender;
+ --  logAddress : VN.VN_Logical_Address;
+
+
+   function Test(logAddress : VN.VN_Logical_Address;
+                 canAddressIn : VN.Communication.CAN.CAN_Address_Sender;
+                expectedIndex : Natural) return boolean is
+      canAddressOut : VN.Communication.CAN.CAN_Address_Sender;
+      found : boolean;
+   begin
+      pack.Insert(table, logAddress, canAddressIn);
+      pack.Search(table, logAddress, canAddressOut, found);
+      if canAddressIn /= canAddressOut or not found or pack.NumberOfEntries(table) /= expectedIndex then
+         Put_Line("ERROR !!! canAddressIn= " & canAddressIn'img & " canAddressOut= "
+                  & canAddressOut'img & " found= " & found'img &
+                    " NumberOfEntries= " & pack.NumberOfEntries(table)'img &
+                    " expectedIndex= " & expectedIndex'img);
+         return false;
+      end if;
+      return true;
+   end Test;
+
+
 begin
 
-   canAddressIn := 13;
-   logAddress := 13;
-   pack.Insert(table, logAddress, canAddressIn);
-   pack.Search(table, logAddress, canAddressOut, found);
+--     passed := passed and Test(13, 13, 1);
+--     passed := passed and Test(12033, 12, 2);
 
-   if canAddressIn /= canAddressOut or not found then
-      Put_Line("ERROR !!! canAddressIn= " & canAddressIn'img & " canAddressOut= " & canAddressOut'img & " found= " & found'img);
+   for i in VN.Communication.CAN.CAN_Address_Sender'Range loop
+      passed := passed and Test(VN.VN_Logical_Address(i), i, Integer(i) + 1);
+   end loop;
+
+   for i in VN.Communication.CAN.CAN_Address_Sender'Range loop
+      passed := passed and Test(VN.VN_Logical_Address(i) + 1000, i,
+                                Integer(i) + 2 + Integer(VN.Communication.CAN.CAN_Address_Sender'Last));
+   end loop;
+
+   for i in VN.Communication.CAN.CAN_Address_Sender'Range loop
+      passed := passed and Test(VN.VN_Logical_Address(i) * 100, i,
+                                Integer(i) + 3 + Integer(VN.Communication.CAN.CAN_Address_Sender'Last) * 2);
+   end loop;
+
+   if passed then
+      Put_Line("Test passed");
+   else
+      Put_Line("Test failed");
    end if;
---   Put_Line("canAddress= " & canAddress'img & " found= " & found'img);
-
-   canAddressIn := 120;
-   logAddress := 12033;
-   pack.Insert(table, logAddress, canAddressIn);
-   pack.Search(table, logAddress, canAddressOut, found);
-
-   if canAddressIn /= canAddressOut or not found then
-      Put_Line("ERROR !!! canAddressIn= " & canAddressIn'img & " canAddressOut= " & canAddressOut'img & " found= " & found'img);
-   end if;
-
-   canAddressIn := 0;
-   logAddress := 234;
-   pack.Insert(table, logAddress, canAddressIn);
-   pack.Search(table, logAddress, canAddressOut, found);
-
-   if canAddressIn /= canAddressOut or not found then
-      Put_Line("ERROR !!! canAddressIn= " & canAddressIn'img & " canAddressOut= " & canAddressOut'img & " found= " & found'img);
-   end if;
-
-   Put_Line("Test completed");
 end Routing_Table_Test;
