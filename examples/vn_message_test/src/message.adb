7d44072e572de36ad8fd4a38e94a290683b64f24
Nils <nils@ubuntu.(none)>
2014-04-07 13:05:38 +0200

    added serialize code

--- examples/vn_message_test/src/message.adb
+++ examples/vn_message_test/src/message.adb
@@ -2,11 +2,19 @@
 
 package body Message is
 
-   procedure Serialize(Message : in VN_Message_Basic; buffer : in out Buffer_Array_Type) is
-
+   procedure Serialize(Message : in VN_Message_Basic; buffer : out Buffer_Array_Type) is
+      tempMsg : VN_Message_Basic := Message;
+      for tempMsg'Address use buffer'Address;
    begin
       null;
    end Serialize;
+
+   procedure DeSerialize(Message : out VN_Message_Basic; buffer : in out Buffer_Array_Type) is
+      tempMsg : VN_Message_Basic;
+      for tempMsg'Address use buffer'Address;
+   begin
+      Message := tempMsg;
+   end DeSerialize;
 
 end Message;
 
