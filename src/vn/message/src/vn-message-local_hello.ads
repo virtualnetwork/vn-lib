1109e715d86ded7964ab67a153a64a1376c23ef2
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-04-08 19:19:54 +0200

    Fixed VN Message Local Hello record type

--- src/vn/message/src/vn-message-local_hello.ads
+++ src/vn/message/src/vn-message-local_hello.ads
@@ -1,7 +1,7 @@
 package VN.Message.Local_Hello is
 
    type VN_Local_Hello_Empty_Payload is Array(1 ..
-                                             MAX_PAYLOAD_SIZE - 19) of
+                                             MAX_PAYLOAD_SIZE - 17) of
                                              Interfaces.Unsigned_8;
 
    type VN_Message_Local_Hello is
@@ -16,7 +16,8 @@ package VN.Message.Local_Hello is
    for VN_Message_Local_Hello use record
       Header            at 0 range (16 + MAX_PAYLOAD_SIZE * 8) ..
                                    (16 + MAX_PAYLOAD_SIZE * 8 + 135);
-      Unknown_Payload   at 0 range 152 .. ((CHECKSUM_SIZE - 1) + MAX_PAYLOAD_SIZE * 8);
+      Unknown_Payload   at 0 range 152 ..
+                                   (CHECKSUM_SIZE - 1 + MAX_PAYLOAD_SIZE * 8);
       CUUID             at 0 range 24 .. 151;
       Component_Type    at 0 range 16 .. 23;
       Checksum          at 0 range 0 .. (CHECKSUM_SIZE - 1);
