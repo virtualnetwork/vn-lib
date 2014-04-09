c0cfe34c6986e6dbbe536097df8fb4c33d437e73
Nils <nils@ubuntu.(none)>
2014-04-09 16:18:47 +0200

    will need to redo this

--- src/firmware/can_driver/src/can_driver.ads
+++ src/firmware/can_driver/src/can_driver.ads
@@ -15,14 +15,32 @@ package CAN_Driver is
 
    type CAN_Message_Physical_Access is access all CAN_Message_Physical;
 
-   procedure Send(msg : CAN_Message_Physical_Access);
-   pragma Import(C, Send, "Send_CAN_Message");
+   procedure CAN_Get_Msg_Filter_Mask(x : Interfaces.C.int; y : Interfaces.C.unsigned_char; z : Interfaces.C.unsigned_char);
+   pragma Import(C, CAN_Get_Msg_Filter_Mask, "MSS_CAN_get_msg_filter_mask");
 
-   function Receive(msg : CAN_Message_Physical_Access) return Interfaces.C.int;
-   pragma Import(C, Receive, "Receive_CAN_Message");
+--CAN_MSGOBJECT
+--CAN_RXMSGOBJECT
+--mss_can_instance_t
+--MSS_CAN_send_message_n(&g_can0, 0, &pMsg);
+--MSS_CAN_get_message_n(&g_can0, 0, &rx_buf)
+--MSS_CAN_init(&g_can0, CAN_SPEED_32M_500K, (PCAN_CONFIG_REG)0, 6, 6);
+--CAN_SPEED_32M_500K
+--PCAN_CONFIG_REG
 
-   function Test return Interfaces.C.int;
-   pragma Import(C, Test, "test");
+--MSS_CAN_set_mode(&g_can0,CANOP_MODE_NORMAL);
+--MSS_CAN_start(&g_can0);
+--MSS_CAN_config_buffer_n(&g_can0, 0, &rx_msg);
+
+
+
+--     procedure Send(msg : CAN_Message_Physical_Access);
+--     pragma Import(C, Send, "Send_CAN_Message");
+--
+--     function Receive(msg : CAN_Message_Physical_Access) return Interfaces.C.int;
+--     pragma Import(C, Receive, "Receive_CAN_Message");
+--
+--     function Test return Interfaces.C.int;
+--     pragma Import(C, Test, "test");
 
    procedure Temp(msg : CAN_Message_Physical);
 
