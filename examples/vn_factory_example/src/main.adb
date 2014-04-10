8917551882f2e7ebc6bc458981a387887251d2d7
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-04-10 09:38:56 +0200

    Updated the VN Factory Example.

--- examples/vn_factory_example/src/main.adb
+++ examples/vn_factory_example/src/main.adb
@@ -1,22 +1,33 @@
 with VN.Message.Factory;
 with VN.Message.Local_Hello;
+use VN.Message.Local_Hello;
+with VN.Message.Local_Ack;
+use VN.Message.Local_Ack;
 with Text_IO;
 
 procedure Main is
-   VN_Access : VN.Message.VN_Message_Access;
---   package VN_Version_Print is new Text_IO.Modular_IO(VN_Message.VN_Version);
-   package VN_CUUID_Print is new Text_IO.Modular_IO(VN.Message.VN_CUUID);
+   use VN.Message;
+
+   package VN_Checksum_Print is new Text_IO.Modular_IO(VN.Message.VN_Checksum);
+   package VN_Opcode_Print is new Text_IO.Modular_IO(VN.Message.VN_Opcode);
+
+   VN_Msg_Basic         : VN.Message.VN_Message_Basic;
+   VN_Msg_Local_Hello   : VN_Message_Local_Hello;
+   VN_Msg_Local_Ack     : VN_Message_Local_Ack;
 begin
-   VN_Access := VN.Message.Factory.Create(VN.Message.Type_Local_Hello);
---   VN_Version_Print.Put(VN_Msg.Get_Version);
---   VN_Msg.Set_Version(1);
---   VN_Version_Print.Put(VN_Msg.Get_Version);
---   VN_Msg.Set_Version(2);
---   VN_Version_Print.Put(VN_Msg.Get_Version);
-   VN.Message.Local_Hello.VN_Message_Local_Hello_Access(VN_Access).Set_CUUID(16#FFFF_FFFF_FFFF_FFFF#);
---   VN_Message.Handler_Local_Hello.Parse(VN_Msg);
---   VN_Msg.Set_CUUID(16#FFFF_FFFF_DEAD_BEAF#);
---   VN_CUUID_Print.Put(VN_Msg.Get_CUUID);
---   VN_Message.Handler_Local_Hello.Parse(VN_Msg);
-   VN.Message.Free(VN_Access);
+   -- Set starting values
+   VN_Msg_Basic.Header.Opcode := 16#0F#;
+   VN_Msg_Basic.Checksum      := 16#FF#;
+
+   -- Print starting values
+   VN_Opcode_Print.Put(VN_Msg_Basic.Header.Opcode);
+   VN_Checksum_Print.Put(VN_Msg_Basic.Checksum);
+   Text_IO.Put_Line("");
+
+   VN_Msg_Basic := VN.Message.Factory.Create(Type_Local_Hello);
+
+   -- Print values after a Local_Hello message was created
+   VN_Opcode_Print.Put(VN_Msg_Basic.Header.Opcode);
+   VN_Checksum_Print.Put(VN_Msg_Basic.Checksum);
+   Text_IO.Put_Line("");
 end Main;
