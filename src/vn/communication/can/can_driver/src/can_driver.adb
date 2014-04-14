78c696455e036c922531c0bbf1c2ed3f43fc85dc
Nils <nils@ubuntu.(none)>
2014-04-14 09:34:06 +0200

    .adb file is currently not used

---
+++ src/vn/communication/can/can_driver/src/can_driver.adb
@@ -0,0 +1,13 @@
+
+package body CAN_Driver is
+
+   procedure Temp(msg : CAN_Message_Physical) is
+   begin
+      null;
+--      MSS_CAN_init
+--  MSS_CAN_set_mode
+--   MSS_CAN_start
+-- MSS_CAN_config_buffer_n
+
+   end Temp;
+end CAN_Driver;
