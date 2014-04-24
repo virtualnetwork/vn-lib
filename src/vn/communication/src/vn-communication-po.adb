5c7c8ec80f9daf748c32397faa6c5adf30d15810
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-04-24 08:54:06 +0000

    Updated Protected objects / IPC communication.

--- src/vn/communication/src/vn-communication-po.adb
+++ src/vn/communication/src/vn-communication-po.adb
@@ -5,29 +5,61 @@ package body VN.Communication.PO is
 
    protected body VN_PO is
 
-      procedure Receive_From_SM_L(Message: out VN.Message.VN_Message_Basic;
-                     Status: out VN.Receive_Status) is
+      -- Procedures used from the SM_L side
+      procedure Send_To_Other(Message: in VN.Message.VN_Message_Basic;
+                              Status: out VN.Send_Status) is
       begin
-         VN_Message_Buffer.Remove(Message, Buffer_To_Other);
-      end Receive_From_SM_L;
-
-      procedure Send_To_SM_L(Message: in VN.Message.VN_Message_Basic;
-                      Status: out VN.Send_Status) is
-      begin
-         VN_Message_Buffer.Insert(Message, Buffer_To_SM_L);
-      end Send_To_SM_L;
+         if VN_Message_Buffer.Full(Buffer_To_Other) then
+            Status := VN.ERROR_BUFFER_FULL;
+         else
+            Status := VN.OK;
+            VN_Message_Buffer.Insert(Message, Buffer_To_Other);
+         end if;
+      end Send_To_Other;
 
       procedure Receive_From_Other(Message: out VN.Message.VN_Message_Basic;
                      Status: out VN.Receive_Status) is
       begin
-         VN_Message_Buffer.Remove(Message, Buffer_To_Other);
+         if VN_Message_Buffer.Empty(Buffer_To_SM_L) then
+            Status := VN.NO_MSG_RECEIVED;
+         else
+            VN_Message_Buffer.Remove(Message, Buffer_To_SM_L);
+
+            if VN_Message_Buffer.Empty(Buffer_To_SM_L) then
+               Status := VN.MSG_RECEIVED_NO_MORE_AVAILABLE;
+            else
+               Status := VN.MSG_RECEIVED_MORE_AVAILABLE;
+            end if;
+         end if;
       end Receive_From_Other;
 
-      procedure Send_To_Other(Message: in VN.Message.VN_Message_Basic;
-                              Status: out VN.Send_Status) is
+      -- Procedures used from the application side
+      procedure Send_To_SM_L(Message: in VN.Message.VN_Message_Basic;
+                      Status: out VN.Send_Status) is
       begin
-         VN_Message_Buffer.Insert(Message, Buffer_To_SM_L);
-      end Send_To_Other;
+         if VN_Message_Buffer.Full(Buffer_To_SM_L) then
+            Status := VN.ERROR_BUFFER_FULL;
+         else
+            Status := VN.OK;
+            VN_Message_Buffer.Insert(Message, Buffer_To_SM_L);
+         end if;
+      end Send_To_SM_L;
+
+      procedure Receive_From_SM_L(Message: out VN.Message.VN_Message_Basic;
+                     Status: out VN.Receive_Status) is
+      begin
+         if VN_Message_Buffer.Empty(Buffer_To_Other) then
+            Status := VN.NO_MSG_RECEIVED;
+         else
+            VN_Message_Buffer.Remove(Message, Buffer_To_Other);
+
+            if VN_Message_Buffer.Empty(Buffer_To_Other) then
+               Status := VN.MSG_RECEIVED_NO_MORE_AVAILABLE;
+            else
+               Status := VN.MSG_RECEIVED_MORE_AVAILABLE;
+            end if;
+         end if;
+      end Receive_From_SM_L;
 
       -- Get buffer length
       function Get_Buffer_Length_To_Other return Integer is
