09b16fd7a3e6559172363efa1f5204613788ef42
Nils <nils@ubuntu.(none)>
2014-04-24 15:16:36 +0200

    more debug output

--- examples/protocol_routing_test/src/protocol_routing_test_main.adb
+++ examples/protocol_routing_test/src/protocol_routing_test_main.adb
@@ -22,14 +22,26 @@ procedure Protocol_Routing_Test_Main is
    now : Ada.Real_Time.Time;
 
 begin
-   now := Ada.Real_Time.Clock;
-   delay until now + Ada.Real_Time.Milliseconds(4000);
+
+   GNAT.IO.New_Line(2);
+   GNAT.IO.Put_Line("Hello world! Protocol_Routing_Test started!");
+
+  -- now := Ada.Real_Time.Clock;
+  -- delay until now + Ada.Real_Time.Milliseconds(4000);
+
+ --  GNAT.IO.Put_Line("4 second wait ended.");
 
    msg.Header.Source := 5;
    msg.Header.Destination := 1337;
 
    Protocol_Routing_Test.myInterface.Send(msg, Status);
 
-   GNAT.IO.Put_Line("Message sent");
+   GNAT.IO.Put_Line("VN message written to send buffer");
 
+   GNAT.IO.Put_Line("Main function entering infinte wait.");
+   loop
+      now := Ada.Real_Time.Clock;
+      delay until now + Ada.Real_Time.Milliseconds(1000);
+      GNAT.IO.Put_Line("<Main function hearbeat>");
+   end loop;
 end Protocol_Routing_Test_Main;
