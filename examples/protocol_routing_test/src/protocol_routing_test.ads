d85e15ba635d50a119cf3cb49acfbc9d6a2f8d15
Nils <nils@ubuntu.(none)>
2014-04-25 10:53:17 +0200

    copyright info added

--- examples/protocol_routing_test/src/protocol_routing_test.ads
+++ examples/protocol_routing_test/src/protocol_routing_test.ads
@@ -1,4 +1,8 @@
-pragma Profile (Ravenscar);
+-- Copyright (c) 2014 All Rights Reserved
+-- Author: Nils Brynedal Ignell
+-- Date: 2014-XX-XX
+-- Summary:
+-- Protocol_Routing_Test is a test for the Protocol_Routing package.
 
 with System;
 with Ada.Real_Time;
