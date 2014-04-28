d0c75ddf36598987a3155c0ea246f5c3f7b94fa0
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-04-28 08:16:39 +0200

    Added Application_Information functionality

--- src/vn/application_information/src/vn-application_information.adb
+++ src/vn/application_information/src/vn-application_information.adb
@@ -1,8 +1,31 @@
+with VN.Message.Assign_Address;
+with VN.Message.Local_Hello;
+
 package body VN.Application_Information is
 
    function Has_Logical_Address return Boolean is
    begin
       return true;
    end Has_Logical_Address;
+
+   procedure Set_Logical_Address(
+            Message: in VN.Message.Assign_Address.VN_Message_Assign_Address) is
+   begin
+      This_Application.Logical_Address := Message.Assigned_Address;
+   end Set_Logical_Address;
+
+   procedure Get_Application_Information(
+            Message: out VN.Message.Local_Hello.VN_Message_Local_Hello) is
+   begin
+      Message.CUUID := This_Application.CUUID;
+      Message.Component_Type := This_Application.Component_Type;
+   end Get_Application_Information;
+
+   procedure Init(CUUID:            in VN.VN_CUUID;
+                  Component_Type:   in VN.Message.VN_Component_Type) is
+   begin
+      This_Application.CUUID := CUUID;
+      This_Application.Component_Type := Component_Type;
+   end Init;
 
 end VN.Application_Information;
--- src/vn/application_information/src/vn-application_information.ads
+++ src/vn/application_information/src/vn-application_information.ads
@@ -1,7 +1,31 @@
+with VN.Message;
+with VN.Message.Assign_Address;
+with VN.Message.Local_Hello;
+
 package VN.Application_Information is
 
-   type VN_app_info is mod 2 ** 16;
-
    function Has_Logical_Address return Boolean;
+
+   procedure Set_Logical_Address(
+            Message: in VN.Message.Assign_Address.VN_Message_Assign_Address);
+
+   procedure Get_Application_Information(
+            Message: out VN.Message.Local_Hello.VN_Message_Local_Hello);
+
+   procedure Init(CUUID:            in VN.VN_CUUID;
+                  Component_Type:   in VN.Message.VN_Component_Type);
+
+   type VN_Application_Information is private;
+
+private
+
+   type VN_Application_Information is
+      record
+         Logical_Address   : VN.VN_Logical_Address := 16#0000_0000#;
+         CUUID             : VN.VN_CUUID; -- TODO: Assign default value.
+         Component_Type    : VN.Message.VN_Component_Type := VN.Message.Other;
+      end record;
+
+   This_Application : VN_Application_Information;
 
 end VN.Application_Information;
