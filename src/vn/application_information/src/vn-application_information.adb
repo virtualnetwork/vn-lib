6aa2fb3e95021cc17c5f75bfd0f0c9bd3ab4b0f3
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-04-28 07:50:24 +0200

    Added basic structure for application information

---
+++ src/vn/application_information/src/vn-application_information.adb
@@ -0,0 +1,8 @@
+package body VN.Application_Information is
+
+   function Has_Logical_Address return Boolean is
+   begin
+      return true;
+   end Has_Logical_Address;
+
+end VN.Application_Information;
---
+++ src/vn/application_information/src/vn-application_information.ads
@@ -0,0 +1,7 @@
+package VN.Application_Information is
+
+   type VN_app_info is mod 2 ** 16;
+
+   function Has_Logical_Address return Boolean;
+
+end VN.Application_Information;
