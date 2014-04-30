3ad8a366202708b5761fb054cb5bae3ddeac4bce
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-04-30 09:55:16 +0200

    Temp commit, basic_vn_application compile and runs

--- examples/basic_vn_application/src/global_settings.ads
+++ examples/basic_vn_application/src/global_settings.ads
@@ -15,15 +15,23 @@ package Global_Settings is
       First_Time: Boolean := True;
    end Start_Time;
 
-   -- Communication between Application and SM-L
+   -- Communication between Application, CAS and SM-L
    PO_To_Application : VN.Communication.PO.VN_PO_Access
                                              := new VN.Communication.PO.VN_PO;
+--   PO_To_CAS         : VN.Communication.PO.VN_PO_Access
+--                                             := new VN.Communication.PO.VN_PO;
+
+   -- Communication object for Application
+   Com_Application   : VN.Communication.IPC.IPC_Wrapper(PO_To_Application,
+                                                           False);
+
+--   -- Communication object for Central Addressing Service
+--   Com_CAS           : VN.Communication.IPC.IPC_Wrapper(PO_To_Application,
+--                                                           False);
 
    -- Communication object for SM-L
-   Com_SM_L         : VN.Communication.IPC.IPC_Wrapper(PO_To_Application,
+   Com_SM_L          : VN.Communication.IPC.IPC_Wrapper(PO_To_Application,
                                                            True);
-   -- Communication object for Application
-   Com_Application  : VN.Communication.IPC.IPC_Wrapper(PO_To_Application,
-                                                           False);
+   -- Com_SM_L          : VN.Communication.Protocol_Router(???);
 
 end Global_Settings;
