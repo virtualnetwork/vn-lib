ef54ce7ce797f71f66dc89a47b54ab68f600406b
Nils <nils@ubuntu.(none)>
2014-05-05 14:37:38 +0200

    Bugfix in Receive function

--- src/vn/communication/protocol_routing/src/vn-communication-protocol_routing.adb
+++ src/vn/communication/protocol_routing/src/vn-communication-protocol_routing.adb
@@ -125,8 +125,10 @@ package body VN.Communication.Protocol_Routing is
 
                Protocol_Router.Search(this.myTable, tempMsg.Header.Destination, address, found);
 
-               if found then
-                  this.Send(tempMsg, sendStatus);
+               if found and address /= 0 then --  address = 0 means send to Application layer
+                  this.myInterfaces(address).Send(tempMsg, sendStatus); --Pass the message on to another subnet
+                  tempStatus := VN.NO_MSG_RECEIVED;
+
                   stop := false;
                else
                   stop := true;
