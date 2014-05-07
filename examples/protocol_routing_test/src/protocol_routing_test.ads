73932cb0e9778fbe1356573040022466d1c57530
Nils <nils@ubuntu.(none)>
2014-05-07 11:10:30 +0200

    Moved Second_Task outside to another package

--- examples/protocol_routing_test/src/protocol_routing_test.ads
+++ examples/protocol_routing_test/src/protocol_routing_test.ads
@@ -31,13 +31,6 @@ package Protocol_Routing_Test is
 
    pragma Elaborate_Body(Protocol_Routing_Test);
 
-   task type Second_Task_Type(myCUUID  : access VN.VN_CUUID;
-                              myAccess : VN.Communication.Com_Access;
-                              Pri : System.Priority;
-                              thePeriod : access Ada.Real_Time.Time_Span) is
-      pragma Priority(Pri);
-   end Second_Task_Type;
-
    theFilter : aliased VN.Communication.CAN.CAN_Filtering.CAN_Filter_Type;
 
    CANPeriod : aliased Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(100);
@@ -67,8 +60,8 @@ package Protocol_Routing_Test is
    PO_Wrapper_3_1 : aliased VN.Communication.PO_Wrapper.VN_PO_Wrapper(PO_1_3'Access, C3'Access, VN.Message.SM_x, false);
    third_PO_Router : aliased VN.Communication.Protocol_Routing.Protocol_Routing_Type;
 
-   secondTask : Second_Task_Type(C2'Access, second_PO_Router'Access, System.Priority'Last, CANPeriod'Access);
-   thirdTask : Second_Task_Type(C3'Access, third_PO_Router'Access, System.Priority'Last, CANPeriod'Access);
+   secondTask : Protocol_Routing_Second_Task.Second_Task_Type(C2'Access, second_PO_Router'Access, System.Priority'Last, CANPeriod'Access);
+   thirdTask  : Protocol_Routing_Second_Task.Second_Task_Type(C3'Access, third_PO_Router'Access, System.Priority'Last, CANPeriod'Access);
 
 private
 
