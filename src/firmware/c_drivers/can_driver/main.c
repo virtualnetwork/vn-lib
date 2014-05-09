ef1c1e388b671a5128623b093ccd7d1903db2ca9
Nils <nils@ubuntu.(none)>
2014-05-09 08:30:50 +0200

    Added code from Magnus

--- src/firmware/c_drivers/can_driver/main.c
+++ src/firmware/c_drivers/can_driver/main.c
@@ -12,23 +12,12 @@
  */
 #include "mss_can.h"
 //#include "drivers/mss_can/mss_can.h"
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 
-/*------------------------------------------------------------------------------
-  Static Variables.
- */
-CAN_FILTEROBJECT pFilter;
-CAN_MSGOBJECT pMsg;
-CAN_MSGOBJECT rx_buf;
-CAN_RXMSGOBJECT rx_msg;
-
-/*------------------------------------------------------------------------------
-  Macros.
- */
-#define   SYSTEM_CLOCK        32000000
-#define   ENTER               0x0D
+CAN_MSGOBJECT tx_msg;
 
 
 typedef struct C_CAN_Msg_T {
@@ -42,33 +31,36 @@ typedef struct C_CAN_Msg_T {
 int test() {return 42;}
 
 int Send_CAN_Message(C_CAN_Message_Type *msg) {
+
+    while( MSS_CAN_send_message_ready(&g_can0) != CAN_OK); //wait until ready
     int i;
 
-    pMsg.ID  = msg->ID;
-    pMsg.DLC = msg->data_length;
+    tx_msg.ID  = msg->ID;
+    tx_msg.DLC = msg->data_length;
 
     for(i=0; i < msg->data_length-1; i++) {
-          pMsg.DATA[i] = msg->DATA[i];
+          tx_msg.DATA[i] = msg->DATA[i];
     }
 
-    pMsg.NA0 = 1; // ToDo. ???????????? "[0..15] Message Valid Bit, 0 == Not valid."
-    pMsg.IDE = 1; //use extended message IDs
-    pMsg.RTR = 1; //regular message (not remote frame)
-    pMsg.NA1 = 0; //padding?
-
-    return MSS_CAN_send_message_n(&g_can0, 6, &pMsg);
+    return MSS_CAN_send_message_n(&g_can0, 6, &tx_msg);
 }
 
 int Receive_CAN_Message(C_CAN_Message_Type *msg) { //returns 1 if message was received, 0 otherwise
 
-    if(CAN_VALID_MSG == MSS_CAN_get_message_n(&g_can0, 0, &rx_buf)) {
-        int i;
+    CAN_MSGOBJECT rx_msg;
+    int i;
 
-        msg->ID = pMsg.ID;
-        msg->data_length = pMsg.DLC;
+    if(CAN_VALID_MSG == MSS_CAN_get_message_av(&g_can0)) {
+
+
+
+        MSS_CAN_get_message(&g_can0, &rx_msg);
+
+        msg->ID = rx_msg.ID;  //??
+        msg->data_length = rx_msg.DLC;//??
 
         for(i=0; i < msg->data_length-1; i++) {
-            msg->DATA[i] = pMsg.DATA[i];
+            msg->DATA[i] = 0xFF & rx_msg.DATA[i];
         }
 
         return 1;
@@ -78,7 +70,8 @@ int Receive_CAN_Message(C_CAN_Message_Type *msg) { //returns 1 if message was re
 }
 
 void Test_Send() {
-    CAN_MSGOBJECT pMsg;
+    ;
+ /*   CAN_MSGOBJECT pMsg;
     pMsg.ID=0x20;
     pMsg.DATALOW = 0x11111111;
     pMsg.DATAHIGH = 0x22222222;
@@ -107,48 +100,71 @@ void Test_Send() {
     MSS_CAN_send_message_n(&g_can0, 16, &pMsg);
     MSS_CAN_send_message_n(&g_can0, 17, &pMsg);
     MSS_CAN_send_message_n(&g_can0, 18, &pMsg);
-    MSS_CAN_send_message_n(&g_can0, 19, &pMsg);
+    MSS_CAN_send_message_n(&g_can0, 19, &pMsg); */
 }
 
 int Init_CAN() {
 
+    int32_t err;
+  //  CAN_CONFIG_REG CANReg;
+    CAN_FILTEROBJECT pFilter;
     int ret;
+
+  /*  CANReg.CFG_BITRATE      = 219;
+    CANReg.CFG_TSEG1        = 12;
+    CANReg.CFG_TSEG2        = 1;
+    CANReg.AUTO_RESTART     = 0;
+    CANReg.CFG_SJW          = 0;
+    CANReg.SAMPLING_MODE    = 0;
+    CANReg.EDGE_MODE        = 0;
+    CANReg.ENDIAN           = 1; */
+
+    tx_msg.ID       = 0x120;
+    tx_msg.DATALOW  = 0x00000000;
+    tx_msg.DATAHIGH = 0x00000000;
+    tx_msg.NA0      = 1;
+    tx_msg.DLC      = 8;
+    tx_msg.IDE      = 1;
+    tx_msg.RTR      = 0;
+
+    printf("\r\n Initializing CAN-Controller... ");
+    fflush(stdout);
+
+    pFilter.ACR.L   = 0x00000000 ;
+    pFilter.AMR.L   = 0xFFFFFFFF;
+    pFilter.AMCR_D.MASK = 0xFFFF;
+    pFilter.AMCR_D.CODE = 0x00;
+
+    err = MSS_CAN_config_buffer(&g_can0, &pFilter);
+    if (err != CAN_OK) {
+        printf("\n\r Message Buffer configuration Error\r\n");
+    }
 
     MSS_CAN_init(&g_can0,
                  CAN_SET_BITRATE(24)|CAN_SET_TSEG1(12)|CAN_SET_TSEG2(1), //CAN_SPEED_32M_50K, //CAN_SPEED_32M_1M,
                  (PCAN_CONFIG_REG)0,
                  6,
                  6);
+    err = MSS_CAN_init(
+        &g_can0,
+        //CAN_SPEED_32M_10K,
+        CAN_SET_BITRATE(24) | CAN_SET_TSEG1(12) | CAN_SET_TSEG2(1), // 100kbit/s ???
+        (PCAN_CONFIG_REG)0,
+        6,
+        6
+    );
+
+    if (err == CAN_OK) {
+        printf("CAN-Controller initialized!\r\n");
+        ret = CAN_OK;
+    }
+    else {
+        printf("Failed initializing CAN-Controller! Error: %ld\r\n\n", err);
+        ret = CAN_OK - 1; //ToDo
+    }
 
     MSS_CAN_set_mode(&g_can0, CANOP_MODE_NORMAL);
-
     MSS_CAN_start(&g_can0);
-
-    /* Configure for receive */
-    /* Initialize the rx mailbox */
-    rx_msg.ID = 0x200;
-    rx_msg.DATAHIGH = 0u;
-    rx_msg.DATALOW = 0u;
-    rx_msg.RXB.DLC = 8u;
-    rx_msg.RXB.IDE = 1;
-    rx_msg.RXB.RTR = 0;
-
-   // rx_msg.AMR.L = 0x00000000; //0xFFFFFFFF;
-    rx_msg.AMR.RTR = 0;
-    rx_msg.AMR.IDE = 1;
-    rx_msg.AMR.ID  = 0;
-
-    rx_msg.ACR.RTR = 0;
-    rx_msg.ACR.IDE = 1;
-    rx_msg.ACR.ID  = 0;
-    //rx_msg.ACR.L = 0x00000000;
-    rx_msg.ACR_D = 0x00000000;
-    rx_msg.AMR_D = 0xFFFFFFFF;
-
-    ret = MSS_CAN_config_buffer_n(&g_can0, 0, &rx_msg);
-
-    MSS_CAN_set_int_ebl(&g_can0, CAN_INT_RX_MSG);
-    MSS_CAN_set_int_ebl(&g_can0, CAN_INT_TX_MSG);
 
     return ret;
 }
