19cbe24cdc57dd0b7f813f644f42be68ec09b274
Nils <nils@ubuntu.(none)>
2014-05-14 19:28:28 +0200

    Added protected objects communication

--- examples/protocol_routing_test/src/protocol_routing_test.adb
+++ examples/protocol_routing_test/src/protocol_routing_test.adb
@@ -25,19 +25,19 @@ package body Protocol_Routing_Test is
    procedure Init is
    begin
 
---        PO_Wrapper_1_2.Init;
---        PO_Wrapper_1_3.Init;
---        PO_Wrapper_2_1.Init;
---        PO_Wrapper_3_1.Init;
+      PO_Wrapper_1_2.Init;
+      PO_Wrapper_1_3.Init;
+      PO_Wrapper_2_1.Init;
+      PO_Wrapper_3_1.Init;
 
---        first_PO_Router.Add_Interface(PO_Wrapper_1_2'Access);
---        first_PO_Router.Add_Interface(PO_Wrapper_1_3'Access);
---
---        second_PO_Router.Add_Interface(PO_Wrapper_2_1'Access);
---        third_PO_Router.Add_Interface(PO_Wrapper_3_1'Access);
+      first_PO_Router.Add_Interface(PO_Wrapper_1_2'Access);
+      first_PO_Router.Add_Interface(PO_Wrapper_1_3'Access);
+
+      second_PO_Router.Add_Interface(PO_Wrapper_2_1'Access);
+      third_PO_Router.Add_Interface(PO_Wrapper_3_1'Access);
 
       myInterface.Add_Interface(CANInterface'Access);
---        myInterface.Add_Interface(first_PO_Router'Access);
+      myInterface.Add_Interface(first_PO_Router'Access);
 
       GNAT.IO.Put_Line("Protocol_Routing_Test Initiated");
    end Init;
