e1ef91e37e7a256ded83110af5c1a1ce323f6acf
Nils <nils@ubuntu.(none)>
2014-05-14 13:33:40 +0200

    minor changes

--- src/vn/communication/can/logic/src/vn-communication-can-logic.ads
+++ src/vn/communication/can/logic/src/vn-communication-can-logic.ads
@@ -17,7 +17,7 @@ with VN.Message;
 
 package VN.Communication.CAN.Logic is
 
-   GIVE_DEBUG_OUTPUT : constant integer := 5;
+   GIVE_DEBUG_OUTPUT : constant integer := 3;
 
    procedure DebugOutput(str : String; level : Integer; newLine : boolean := true);
 
