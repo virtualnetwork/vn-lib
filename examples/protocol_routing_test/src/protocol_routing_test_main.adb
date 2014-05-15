e9508dbff0c01462c1fc46643c84aa18487e2634
Nils <nils@ubuntu.(none)>
2014-05-15 13:40:57 +0200

    Added code for Distribute route etc

--- examples/protocol_routing_test/src/protocol_routing_test_main.adb
+++ examples/protocol_routing_test/src/protocol_routing_test_main.adb
@@ -21,34 +21,44 @@ with VN.Message.Local_Hello;
 with VN.Message.Assign_Address;
 with VN.Message.Request_Address_Block;
 with VN.Message.Factory;
+with VN.Message.Distribute_Route;
 
 with Protocol_Routing_Test;
 
 
 procedure Protocol_Routing_Test_Main is
 
+   type Address_Element is
+      record
+         address 	: VN.VN_Logical_Address;
+         CUUID 		: VN.VN_CUUID;
+         compType	: VN.Message.VN_Component_Type;
+        -- exists 	: boolean := false;
+      end record;
+   type Address_Table is array(1..20) of Address_Element;
 
-   msg	     : VN.Message.VN_Message_Basic;
+   myTable : Address_Table;
+   numAddresses : Integer := 0;
+
+   msg, msg2 : VN.Message.VN_Message_Basic;
    msgAssign : VN.Message.Assign_Address.VN_Message_Assign_Address;
    msgReq    : VN.Message.Request_Address_Block.VN_Message_Request_Address_Block;
-   sendStatus    : VN.Send_Status;
-   recStatus : VN.Receive_Status;
+   msgRoute  : VN.Message.Distribute_Route.VN_Message_Distribute_Route;
+
+   sendStatus : VN.Send_Status;
+   recStatus  : VN.Receive_Status;
 
    msgLocalHello : VN.Message.Local_Hello.VN_Message_Local_Hello;
    msgReqAddrBlock   : VN.Message.Request_Address_Block.VN_Message_Request_Address_Block;
 
    now : Ada.Real_Time.Time;
+   myAddress : VN.VN_Logical_Address;
 
    use VN; -- for if Status = VN.OK then
 begin
 
    GNAT.IO.New_Line(2);
    GNAT.IO.Put_Line("Hello world! Protocol_Routing_Test started!");
-
---     now := Ada.Real_Time.Clock;
---     delay until now + Ada.Real_Time.Milliseconds(5000);
-
-  -- GNAT.IO.Put_Line("5 second wait ended.");
 
    VN.Message.Assign_Address.To_Assign_Address
      (VN.Message.Factory.Create(VN.Message.Type_Assign_Address), msgAssign);
@@ -65,46 +75,15 @@ begin
    msgAssign.CUUID := (others => 42);
    msgAssign.Assigned_Address := 1;
 
---  -------------------
-
---     VN.Message.Assign_Address.To_Basic(msgAssign, msg);
---     Protocol_Routing_Test.myInterface.Send(msg, Status);
---
---     GNAT.IO.Put("VN message written to send buffer");
---     if Status = VN.OK then
---        GNAT.IO.Put(", Status = VN.OK");
---
---     elsif Status = VN.ERROR_BUFFER_FULL then
---        GNAT.IO.Put(", Status = VN.ERROR_BUFFER_FULL");
---
---     elsif Status = VN.ERROR_NO_ADDRESS_RECEIVED then
---        GNAT.IO.Put(", Status = VN.ERROR_NO_ADDRESS_RECEIVED");
---     end if;
---     GNAT.IO.Put_Line("");
-
---  -------------------
-
---     VN.Message.Request_Address_Block.To_Basic(msgReq, msg);
---     Protocol_Routing_Test.myInterface.Send(msg, sendStatus);
---
---     GNAT.IO.Put("VN message written to send buffer");
---     if Status = VN.OK then
---        GNAT.IO.Put(", Status = VN.OK");
---
---     elsif Status = VN.ERROR_BUFFER_FULL then
---        GNAT.IO.Put(", Status = VN.ERROR_BUFFER_FULL");
---
---     elsif Status = VN.ERROR_NO_ADDRESS_RECEIVED then
---        GNAT.IO.Put(", Status = VN.ERROR_NO_ADDRESS_RECEIVED");
---     end if;
---     GNAT.IO.Put_Line("");
---  -------------------
+   if Protocol_Routing_Test.Temp = 0 then
+      myAddress := 100 + VN.VN_Logical_Address(Protocol_Routing_Test.C1(1));
+   end if;
 
    GNAT.IO.Put_Line("Main function entering infinte loop.");
    loop
       now := Ada.Real_Time.Clock;
       delay until now + Ada.Real_Time.Milliseconds(500);
-  --    GNAT.IO.Put_Line("<Main function hearbeat>");
+      GNAT.IO.Put_Line("<Main function hearbeat>");
 
       Protocol_Routing_Test.myInterface.Receive(msg, recStatus);
 
@@ -129,19 +108,30 @@ begin
                VN.Text_IO.Put_Line("Other");
             end if;
 
-            msg := VN.Message.Factory.Create(VN.Message.Type_Assign_Address);
-            VN.Message.Assign_Address.To_Assign_Address(msg, msgAssign);
+            if Protocol_Routing_Test.Temp = 0 then
+               msg := VN.Message.Factory.Create(VN.Message.Type_Assign_Address);
+               VN.Message.Assign_Address.To_Assign_Address(msg, msgAssign);
 
-            msgAssign.Header.Destination := VN.LOGICAL_ADDRES_UNKNOWN;
-            msgAssign.Header.Source := 10;
-            msgAssign.CUUID := msgLocalHello.CUUID;
-            msgAssign.Assigned_Address := 20 + VN.VN_Logical_Address(msgLocalHello.CUUID(1));
+               msgAssign.Header.Destination := VN.LOGICAL_ADDRES_UNKNOWN;
+               msgAssign.Header.Source := myAddress;
+               msgAssign.CUUID := msgLocalHello.CUUID;
+               msgAssign.Assigned_Address := 100 + VN.VN_Logical_Address(msgLocalHello.CUUID(1));
 
-            VN.Message.Assign_Address.To_Basic(msgAssign, msg);
-            Protocol_Routing_Test.myInterface.Send(msg, sendStatus);
+               VN.Message.Assign_Address.To_Basic(msgAssign, msg);
+               Protocol_Routing_Test.myInterface.Send(msg, sendStatus);
 
-            VN.Text_IO.Put_Line("SM_L assinged Logical address " & msgAssign.Assigned_Address'Img &
-              " to CUUD(1)= " & msgAssign.CUUID(1)'Img);
+               VN.Text_IO.Put_Line("SM_L assinged Logical address " & msgAssign.Assigned_Address'Img &
+                                     " to CUUD(1)= " & msgAssign.CUUID(1)'Img);
+
+               -- store the assigned address:
+
+               --              myTable(numAddresses + 1).exists   := true;
+               numAddresses := numAddresses + 1;
+               myTable(numAddresses).CUUID    := msgAssign.CUUID;
+               myTable(numAddresses).compType := msgLocalHello.Component_Type;
+               myTable(numAddresses).address  := msgAssign.Assigned_Address;
+            end if;
+
 
          elsif msg.Header.Opcode = VN.Message.OPCODE_REQUEST_ADDR_BLOCK then
 
@@ -150,6 +140,28 @@ begin
             VN.Text_IO.Put_Line("Request_Address_Block received, CUUID(1)= " & msgReqAddrBlock.CUUID(1)'img &
                           " Sender= " & msgReqAddrBlock.Header.Source'Img & " Sent to " & msgReqAddrBlock.Header.Destination'Img);
             VN.Text_IO.New_Line;
+
+
+            if Protocol_Routing_Test.Temp = 0 then
+               msg2 := VN.Message.Factory.Create(VN.Message.Type_Distribute_Route);
+               VN.Message.Distribute_Route.To_Distribute_Route(msg2, msgRoute);
+
+               msgRoute.Header.Source := myAddress;
+
+               for i in 1 .. numAddresses loop
+                  -- if myTable(i).exists then
+                  msgRoute.CUUID := myTable(i).CUUID;
+                  msgRoute.Component_Type    := myTable(i).compType;
+                  msgRoute.Component_Address := myTable(i).address;
+
+                  VN.Message.Distribute_Route.To_Basic(msgRoute, msg2);
+                  Protocol_Routing_Test.myInterface.Send(msg2, sendStatus);
+
+                  VN.Text_IO.Put_Line("Sening Route regarding log addr " & msgRoute.Component_Address'Img &
+                                        " to " & msgRoute.Header.Destination'Img);
+                  --   end if;
+               end loop;
+            end if;
 
          elsif msg.Header.Opcode = VN.Message.OPCODE_ASSIGN_ADDR then
             VN.Text_IO.Put_Line("Assign address message received from logical address " & msg.Header.Source'Img);
