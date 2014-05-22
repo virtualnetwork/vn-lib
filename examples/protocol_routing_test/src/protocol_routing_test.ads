46511ad62f0bd3f87ab3610880db446755271d72
Nils <nils@ubuntu.(none)>
2014-05-22 13:03:57 +0200

    Minor changes

--- examples/protocol_routing_test/src/protocol_routing_test.ads
+++ examples/protocol_routing_test/src/protocol_routing_test.ads
@@ -34,7 +34,7 @@ package Protocol_Routing_Test is
 
    CANPeriod : aliased Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(10);
 
-   Temp : constant Integer := 0;
+   Temp : constant Integer := 3;
 
    U1 : aliased VN.Communication.CAN.UCID := VN.Communication.CAN.UCID(Temp * 10);
    C1 : aliased VN.VN_CUUID := (Interfaces.Unsigned_8(1 + Temp * 10), others => 5);
@@ -44,7 +44,7 @@ package Protocol_Routing_Test is
 
    CANInterface : aliased VN.Communication.CAN.CAN_Interface.CAN_Interface_Type
      (U1'Unchecked_Access, C1'Unchecked_Access,
-      theFilter'Unchecked_Access, VN.Communication.CAN.CAN_Interface.SM_CAN);
+      theFilter'Unchecked_Access, VN.Communication.CAN.CAN_Interface.Node);
 
    myTask : aliased VN.Communication.CAN.Can_Task.CAN_Task_Type
      (CANInterface'Access, System.Priority'Last, CANPeriod'Access, theFilter'Unchecked_Access);
@@ -55,14 +55,14 @@ package Protocol_Routing_Test is
 
    PO_Wrapper_1_2 : aliased VN.Communication.PO_Wrapper.VN_PO_Wrapper(PO_1_2'Access, C1'Access, VN.Message.SM_L, true);
    PO_Wrapper_1_3 : aliased VN.Communication.PO_Wrapper.VN_PO_Wrapper(PO_1_3'Access, C1'Access, VN.Message.SM_L, true);
-   --     first_PO_Router : aliased VN.Communication.Protocol_Routing.Protocol_Routing_Type;
-   --
-   PO_Wrapper_2_1 : aliased VN.Communication.PO_Wrapper.VN_PO_Wrapper(PO_1_2'Access, C2'Access, VN.Message.SM_x, false);
-   --     second_PO_Router : aliased VN.Communication.Protocol_Routing.Protocol_Routing_Type;
-   --
-   PO_Wrapper_3_1 : aliased VN.Communication.PO_Wrapper.VN_PO_Wrapper(PO_1_3'Access, C3'Access, VN.Message.SM_x, false);
-   --     third_PO_Router : aliased VN.Communication.Protocol_Routing.Protocol_Routing_Type;
-   --
+--     first_PO_Router : aliased VN.Communication.Protocol_Routing.Protocol_Routing_Type;
+
+   PO_Wrapper_2_1 : aliased VN.Communication.PO_Wrapper.VN_PO_Wrapper(PO_1_2'Access, C2'Access, VN.Message.Other, false);
+--     second_PO_Router : aliased VN.Communication.Protocol_Routing.Protocol_Routing_Type;
+
+   PO_Wrapper_3_1 : aliased VN.Communication.PO_Wrapper.VN_PO_Wrapper(PO_1_3'Access, C3'Access, VN.Message.Other, false);
+--     third_PO_Router : aliased VN.Communication.Protocol_Routing.Protocol_Routing_Type;
+--
 --     secondTask : Protocol_Routing_Second_Task.Second_Task_Type(C2'Access, second_PO_Router'Access, System.Priority'Last, CANPeriod'Access);
 --     thirdTask  : Protocol_Routing_Second_Task.Second_Task_Type(C3'Access, third_PO_Router'Access, System.Priority'Last, CANPeriod'Access);
 
