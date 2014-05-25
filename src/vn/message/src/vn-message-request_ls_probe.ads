4374a3d40f150a88318dcd7ab37e5c36b139d5d5
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-05-25 13:36:19 +0000

    Fixed Request_LS_Probe message

--- src/vn/message/src/vn-message-request_ls_probe.ads
+++ src/vn/message/src/vn-message-request_ls_probe.ads
@@ -1,7 +1,7 @@
 package VN.Message.Request_LS_Probe is
 
    REQUEST_LS_PROBE_UNKNOWN_PAYLOAD_SIZE :
-                                    constant integer := MAX_PAYLOAD_SIZE - CUUID_SIZE;
+                                    constant integer := MAX_PAYLOAD_SIZE - VN_LOGICAL_ADDRESS_SIZE;
 
    type VN_Request_LS_Probe_Unknown_Payload is Array(1 ..
                               REQUEST_LS_PROBE_UNKNOWN_PAYLOAD_SIZE) of
@@ -11,22 +11,22 @@ package VN.Message.Request_LS_Probe is
       record
          Header                  : VN_Header;
          Unknown_Payload         : VN_Request_LS_Probe_Unknown_Payload;
-         CUUID                   : VN_CUUID;
+         Component_Address       : VN_Logical_Address;
          Checksum                : VN_Checksum;
       end record;
 
    for VN_Message_Request_LS_Probe use record
       Header            at 0 range 0 .. HEADER_SIZE * 8 - 1;
 
-      CUUID             at 0 range HEADER_SIZE * 8 ..
-                                   (HEADER_SIZE + CUUID_SIZE) * 8 - 1;
+      Component_Address at 0 range HEADER_SIZE * 8 ..
+                                   (HEADER_SIZE + VN_LOGICAL_ADDRESS_SIZE) * 8 - 1;
 
-      Unknown_Payload   at 0 range (HEADER_SIZE + CUUID_SIZE) * 8 ..
-        (HEADER_SIZE + CUUID_SIZE + REQUEST_LS_PROBE_UNKNOWN_PAYLOAD_SIZE) * 8 - 1;
+      Unknown_Payload   at 0 range (HEADER_SIZE + VN_LOGICAL_ADDRESS_SIZE) * 8 ..
+        (HEADER_SIZE + VN_LOGICAL_ADDRESS_SIZE + REQUEST_LS_PROBE_UNKNOWN_PAYLOAD_SIZE) * 8 - 1;
 
-      Checksum          at 0 range (HEADER_SIZE + CUUID_SIZE +
+      Checksum          at 0 range (HEADER_SIZE + VN_LOGICAL_ADDRESS_SIZE +
                                       REQUEST_LS_PROBE_UNKNOWN_PAYLOAD_SIZE) * 8 ..
-          (HEADER_SIZE + CUUID_SIZE + REQUEST_LS_PROBE_UNKNOWN_PAYLOAD_SIZE + CHECKSUM_SIZE) * 8 - 1;
+          (HEADER_SIZE + VN_LOGICAL_ADDRESS_SIZE + REQUEST_LS_PROBE_UNKNOWN_PAYLOAD_SIZE + CHECKSUM_SIZE) * 8 - 1;
    end record;
 
    for VN_Message_Request_LS_Probe'Alignment use 2;
