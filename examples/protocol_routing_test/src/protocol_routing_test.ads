6ce94ce95fe87ddfb381941b99ce7701ca00953d
Nils <nils@ubuntu.(none)>
2014-05-25 11:31:09 +0200

    Minor changes

--- examples/protocol_routing_test/src/protocol_routing_test.ads
+++ examples/protocol_routing_test/src/protocol_routing_test.ads
@@ -34,17 +34,17 @@ package Protocol_Routing_Test is
 
    CANPeriod : aliased Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(10);
 
-   Temp : constant Integer := 2;
+   identifier : constant Integer := 2;
 
-   U1 : aliased VN.Communication.CAN.UCID := VN.Communication.CAN.UCID(Temp * 10);
-   C1 : aliased VN.VN_CUUID := (Interfaces.Unsigned_8(1 + Temp * 10), others => 5);
+   U1 : aliased VN.Communication.CAN.UCID := VN.Communication.CAN.UCID(identifier * 10);
+   C1 : aliased VN.VN_CUUID := (Interfaces.Unsigned_8(1 + identifier * 10), others => 5);
 
-   C2 : aliased VN.VN_CUUID := (Interfaces.Unsigned_8(2 + Temp * 10), others => 5);
-   C3 : aliased VN.VN_CUUID := (Interfaces.Unsigned_8(3 + Temp * 10), others => 5);
+   C2 : aliased VN.VN_CUUID := (Interfaces.Unsigned_8(2 + identifier * 10), others => 5);
+   C3 : aliased VN.VN_CUUID := (Interfaces.Unsigned_8(3 + identifier * 10), others => 5);
 
    CANInterface : aliased VN.Communication.CAN.CAN_Interface.CAN_Interface_Type
      (U1'Unchecked_Access, C1'Unchecked_Access,
-      theFilter'Unchecked_Access, VN.Communication.CAN.CAN_Interface.Node);
+      theFilter'Unchecked_Access, VN.Communication.CAN.CAN_Interface.SM_CAN);
 
    myTask : aliased VN.Communication.CAN.Can_Task.CAN_Task_Type
      (CANInterface'Access, System.Priority'Last, CANPeriod'Access, theFilter'Unchecked_Access);
@@ -59,13 +59,15 @@ package Protocol_Routing_Test is
    PO_Wrapper_2_1 : aliased VN.Communication.PO_Wrapper.VN_PO_Wrapper(PO_1_2'Access, C2'Access, VN.Message.Other, false);
    PO_Wrapper_3_1 : aliased VN.Communication.PO_Wrapper.VN_PO_Wrapper(PO_1_3'Access, C3'Access, VN.Message.Other, false);
 
+
+-- **** Code for adding the application threads ***
 --     first_PO_Router : aliased VN.Communication.Protocol_Routing.Protocol_Routing_Type;
 --     second_PO_Router : aliased VN.Communication.Protocol_Routing.Protocol_Routing_Type;
 --     third_PO_Router : aliased VN.Communication.Protocol_Routing.Protocol_Routing_Type;
 --
 --     secondTask : Protocol_Routing_Second_Task.Second_Task_Type(C2'Access, second_PO_Router'Access, System.Priority'Last, CANPeriod'Access);
 --     thirdTask  : Protocol_Routing_Second_Task.Second_Task_Type(C3'Access, third_PO_Router'Access, System.Priority'Last, CANPeriod'Access);
-
+-- ***** ******************************
 private
 
    procedure Init;
