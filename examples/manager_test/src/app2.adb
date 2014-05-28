319ba54ce0f2b5e42728203a4672d8c25a940fd9
Nils <nils@ubuntu.(none)>
2014-05-28 11:48:51 +0200

    Removed some debug output and removed exit from loop

--- examples/manager_test/src/app2.adb
+++ examples/manager_test/src/app2.adb
@@ -60,7 +60,8 @@ package body App2 is
          Global_Settings.Com_App2.Receive(Basic_Msg, Recv_Status);
 
          if Recv_Status = VN.NO_MSG_RECEIVED then
-            VN.Text_IO.Put_Line("APP2 RECV: Empty.");
+            --              VN.Text_IO.Put_Line("APP2 RECV: Empty.");
+            null;
          elsif Recv_Status = VN.MSG_RECEIVED_NO_MORE_AVAILABLE or
             Recv_Status = VN.MSG_RECEIVED_MORE_AVAILABLE    then
 
@@ -98,8 +99,8 @@ package body App2 is
 
 
          Next_Period := Next_Period + Period;
-         i := i + 1;
-         exit when i = 15;
+--           i := i + 1;
+--           exit when i = 15;
       end loop;
       ----------------------------
 
