860c7d19a38cfc96028f7e513722315ab13e7bd9
Nils <nils@ubuntu.(none)>
2014-05-28 11:56:27 +0200

    Changed debug output

--- src/vn/communication/can/logic/node/src/vn-communication-can-logic-node.adb
+++ src/vn/communication/can/logic/node/src/vn-communication-can-logic-node.adb
@@ -137,8 +137,8 @@ package body VN.Communication.CAN.Logic.Node is
          CAN_Routing.Search(this.myTable, msg.Header.Destination, receiver, found, isDirect'Access);
 
          if isDirect then
-            VN.Text_IO.Put_Line("CAN routing: Sending VN message via direct routing. Destination " & msg.Header.Destination'Img &
-                                  " CAN address = " & receiver'Img);
+            VN.Communication.CAN.Logic.DebugOutput("CAN routing: Sending VN message via direct routing. Destination " & msg.Header.Destination'Img &
+                                                     " CAN address = " & receiver'Img, 1);
          end if;
       end if;
 
