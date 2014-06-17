f716bf3dacd422bc4a206ecd78911ca1fdc5ada6
Nils Brynedal Ignell <nils.brynedal@gmail.com>
2014-06-17 14:04:12 +0200

    Updated task_test so that it is up to date

--- examples/task_test2/src/task_test.ads
+++ examples/task_test2/src/task_test.ads
@@ -6,19 +6,24 @@
 
 With VN.Communication.CAN.Can_Task;
 with VN.Communication.CAN.CAN_Interface;
+with VN.Communication.CAN.CAN_Filtering;
+
 with Ada.Real_Time;
 with System;
 
 package Task_Test is
+
+   theFilter : aliased VN.Communication.CAN.CAN_Filtering.CAN_Filter_Type;
 
    thePeriod : aliased Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(100);
    U1 : aliased VN.Communication.CAN.UCID := 1;
    C1 : aliased VN.VN_CUUID := (1, others => 5);
 
    myInterface : aliased VN.Communication.CAN.CAN_Interface.CAN_Interface_Type
-     (U1'Unchecked_Access, C1'Unchecked_Access, VN.Communication.CAN.CAN_Interface.SM_CAN);
+     (U1'Unchecked_Access, C1'Unchecked_Access, theFilter'Unchecked_Access, 
+      VN.Communication.CAN.CAN_Interface.SM_CAN);
 
    myTask : aliased VN.Communication.CAN.Can_Task.CAN_Task_Type
-     (myInterface'Access, 7, thePeriod'Access);
+     (myInterface'Access, 7, thePeriod'Access, theFilter'Unchecked_Access);
 
 end Task_Test;
--- examples/task_test2/src/task_test_main.adb
+++ examples/task_test2/src/task_test_main.adb
@@ -12,38 +12,22 @@ with System.Machine_Code;
 with Ada.Real_Time;
 use Ada.Real_Time;
 
---  with Task_Test;
-
-with Test_No_Tasks;
+with Task_Test;
 
 with GNAT.IO;
 
 procedure Task_Test_Main is
-   pragma Priority (3);
 
    t : Ada.Real_Time.Time;
 
 begin
 
-
---     t := Ada.Real_Time.Clock;
---     delay until t + Ada.Real_Time.Seconds(7);
-
-   GNAT.IO.Put_Line("Hello world, Task_Test_Main started! Doing hard coded test");
+   GNAT.IO.Put_Line("Hello world, Task_Test_Main started!");
 
    t := Ada.Real_Time.Clock;
    loop
 
-
-
       t := t + Ada.Real_Time.Milliseconds(1000);
       delay until t;
---        while Clock < t loop
---           System.Machine_Code.Asm ("wfe", Volatile => True);
---           null;
---        end loop;
-
-      Test_No_Tasks.Update;
-
    end loop;
 end Task_Test_Main;
