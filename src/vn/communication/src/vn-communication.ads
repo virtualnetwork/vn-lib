0b355c5c07b683c15e9845488fab1690c97bd637
Nils Brynedal Ignell <nils.brynedal@gmail.com>
2014-06-17 14:13:32 +0200

    added comment regarding Com interface

--- src/vn/communication/src/vn-communication.ads
+++ src/vn/communication/src/vn-communication.ads
@@ -6,7 +6,7 @@ package VN.Communication is
    package VN_Message_Buffer is
          new Buffers(VN.Message.VN_Message_Basic);
 
-   -- The Com interface is inherited by all subbnet protocols and by the Protocol routing layer.
+   -- The Com interface is inherited by all subnet protocols and by the Protocol routing layer.
    type Com is limited interface;
 
    type Com_Access is access all Com'Class;
