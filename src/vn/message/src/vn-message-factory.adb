77fd820125e9e50ef80fa60b50d428deef415125
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-06-17 18:13:52 +0200

    Added GPL 3 license to VN.Message package

--- src/vn/message/src/vn-message-factory.adb
+++ src/vn/message/src/vn-message-factory.adb
@@ -1,3 +1,22 @@
+------------------------------------------------------------------------------
+--  This file is part of VN-Lib.
+--
+--  VN-Lib is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  VN-Lib is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with VN-Lib.  If not, see <http://www.gnu.org/licenses/>.
+--
+--  Copyright 2014 Christoffer Holmstedt (christoffer.holmstedt@gmail.com)
+------------------------------------------------------------------------------
+
 with VN.Message.Local_Hello;
 use VN.Message.Local_Hello;
 with VN.Message.Local_Ack;
